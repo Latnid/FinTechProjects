a14ca3ee3f657ca799d7ee44cbe0d561c81543c2
Boots_ <connorboots@gmail.com>
2022-09-14 23:17:27 -0500

    fixed paramater issue

--- One/qualifier/utils/SQL.py
+++ One/qualifier/utils/SQL.py
@@ -1,16 +1,14 @@
 from Modules.CleanData import get_data
 
 
-def add_new_table():
+def add_new_table(data):
     # Importing the required libraries and dependencies
     import numpy as np
     import pandas as pd
     import hvplot.pandas
     import sqlalchemy
 
-
-
-    table_date = a['Time'].iloc[0] 
+    table_date = data['Time'].iloc[0] 
     # Create a temporary sqlite database
     
     database_connection_string = 'sqlite:///options.db'
@@ -20,11 +18,10 @@ def add_new_table():
         echo=True
     )
 
-    a.to_sql(table_date,con = engine, index=False, if_exists='replace')
+    data.to_sql(table_date,con = engine, index=False, if_exists='replace')
     # CREATE TABLE table_date (
     #     'Symbol','Type','Strike','Open Int','Volume','OI Chg','IV'
     #     ...
     # )
-
     
     return engine.table_names()
