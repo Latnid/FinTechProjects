e9a66b962ec62a53fa9447b9bb1a70e5441d2f90
Boots_ <connorboots@gmail.com>
2022-09-14 21:38:27 -0500

    updated alpaca.py

---
+++ One/qualifier/utils/alpaca_api.py
@@ -0,0 +1,30 @@
+from email import message
+import os
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
+import numpy as np
+import alpaca_trade_api as alpaca
+
+def alpaca_func(API_KEY,API_SECRET):
+    alpaca_key = os.getenv(API_KEY)
+    alpaca_secret = os.getenv(API_SECRET)
+
+    api_alpaca = alpaca.REST(key_id=alpaca_key, secret_key=alpaca_secret, api_version="v2")
+
+    df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start="2019-01-01", end="2022-05-30").df
+
+    fb_df = df[df["symbol"]=="FB"].drop(columns="symbol",axis = 1)
+
+    msft_df = df[df["symbol"]=="MSFT"].drop(columns="symbol",axis = 1)
+
+    aapl_df = df[df["symbol"]=="AAPL"].drop(columns="symbol",axis = 1)
+
+    fb_msft_aapl = pd.concat([fb_df, msft_df, aapl_df], axis = 1, keys = ["FB","MSFT", "AAPL"])
+
+    plot = fb_msft_aapl["MSFT"]["close"].plot()
+
+    message = print(f"Successfully connected to API...")
+
+    return message
