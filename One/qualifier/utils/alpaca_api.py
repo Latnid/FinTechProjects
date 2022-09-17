bfbf60d8f55be16701accd2eeadc8256cc342d47
Boots_ <connorboots@gmail.com>
2022-09-16 23:36:43 -0500

    trying to fix issue with key and secret

--- One/qualifier/utils/alpaca_api.py
+++ One/qualifier/utils/alpaca_api.py
@@ -11,27 +11,30 @@ import questionary
 # START_DATE = questionary.text("What's your start date?").ask()
 # END_DATE = questionary.text("What's your end date?").ask()
 
-def alpaca_func(START_DATE,END_DATE):
+alpaca_key = os.getenv("ALPACA_API_KEY")
+alpaca_secret = os.getenv("ALPACA_SECRET_KEY")
 
-    # Gets key and secret.
-    alpaca_key = os.getenv("ALPACA_API_KEY")
-    alpaca_secret = os.getenv("ALPACA_API_SECRET")
+def alpaca_func(alpaca_key, alpaca_secret):
+
+    # Creates connection.
     api_alpaca = alpaca.REST(key_id=alpaca_key, secret_key=alpaca_secret, api_version="v2")
 
     # Specifies tickers to get from Alpaca API.
     # df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start="2022-01-01", end="2022-05-30").df
-    df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start=START_DATE, end=END_DATE).df
+    df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start="2019-01-01", end="2022-05-30").df
 
     # Specifies which companies to get data for.
     fb_df = df[df["symbol"]=="FB"].drop(columns="symbol",axis = 1)
+
     msft_df = df[df["symbol"]=="MSFT"].drop(columns="symbol",axis = 1)
+
     aapl_df = df[df["symbol"]=="AAPL"].drop(columns="symbol",axis = 1)
     
     # Concat data together.
-    fb_msft_aapl = pd.concat([fb_df, msft_df, aapl_df], axis = 1, keys = ["FB","MSFT", "AAPL"])
+    fb_msft = pd.concat([fb_df, msft_df, aapl_df], axis = 1, keys = ["FB","MSFT", "AAPL"])
 
     # Plots the data frame.
-    # plot_MSFT = fb_msft_aapl["MSFT"]["close"].plot()
+    # plot_AAPL = aapl_df["AAPL"]["close"].plot()
 
     # Prints a message.
     message = print(f"Successfully gathered data from API.")
