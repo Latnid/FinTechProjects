81a9e2e7a4cbec11c879c791c1d0482470133206
Boots_ <connorboots@gmail.com>
2022-09-16 21:45:53 -0500

    updated alpaca_api.py arguments

--- One/qualifier/utils/alpaca_api.py
+++ One/qualifier/utils/alpaca_api.py
@@ -6,7 +6,7 @@ load_dotenv()
 import numpy as np
 import alpaca_trade_api as alpaca
 
-def alpaca_func(API_KEY,API_SECRET,START,END):
+def alpaca_func(API_KEY,API_SECRET,START_DATE,END_DATE):
     # Gets key and secret.
     alpaca_key = os.getenv(API_KEY)
     alpaca_secret = os.getenv(API_SECRET)
@@ -14,7 +14,7 @@ def alpaca_func(API_KEY,API_SECRET,START,END):
 
     # Specifies tickers to get from Alpaca API.
     # df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start="2022-01-01", end="2022-05-30").df
-    df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start=START, end=END).df
+    df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start=START_DATE, end=END_DATE).df
 
     # Specifies which companies to get data for.
     fb_df = df[df["symbol"]=="FB"].drop(columns="symbol",axis = 1)
@@ -25,7 +25,7 @@ def alpaca_func(API_KEY,API_SECRET,START,END):
     fb_msft_aapl = pd.concat([fb_df, msft_df, aapl_df], axis = 1, keys = ["FB","MSFT", "AAPL"])
 
     # Plots the data frame.
-    plot = fb_msft_aapl["MSFT"]["close"].plot()
+    plot_MSFT = fb_msft_aapl["MSFT"]["close"].plot()
 
     # Prints a message.
     message = print(f"Successfully connected to API...")
