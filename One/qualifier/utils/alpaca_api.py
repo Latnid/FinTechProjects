1e5f0f0e8a1f8fde559f44c036042f56f437dbb4
Boots_ <connorboots@gmail.com>
2022-09-17 20:50:17 -0500

    changed FB to META in alpaca_api.py

--- One/qualifier/utils/alpaca_api.py
+++ One/qualifier/utils/alpaca_api.py
@@ -18,17 +18,17 @@ def alpaca_func(alpaca_key, alpaca_secret,start_date,end_date):
 
     # Specifies tickers to get from Alpaca API.
     # df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start="2022-01-01", end="2022-05-30").df
-    df = api_alpaca.get_bars(symbol=["FB","MSFT", "AAPL"], timeframe="1D", start=start_date, end=end_date).df
+    df = api_alpaca.get_bars(symbol=["META","MSFT", "AAPL"], timeframe="1D", start=start_date, end=end_date).df
 
     # Specifies which companies to get data for.
-    fb_df = df[df["symbol"]=="FB"].drop(columns="symbol",axis = 1)
+    meta_df = df[df["symbol"]=="META"].drop(columns="symbol",axis = 1)
 
     msft_df = df[df["symbol"]=="MSFT"].drop(columns="symbol",axis = 1)
 
     aapl_df = df[df["symbol"]=="AAPL"].drop(columns="symbol",axis = 1)
     
     # Concat data together.
-    fb_msft_aapl = pd.concat([fb_df, msft_df, aapl_df], axis = 1, keys = ["FB","MSFT", "AAPL"])
+    meta_msft_aapl = pd.concat([meta_df, msft_df, aapl_df], axis = 1, keys = ["META","MSFT", "AAPL"])
 
     # Plots the data frame.
     # plot_AAPL = aapl_df["AAPL"]["close"].plot()
